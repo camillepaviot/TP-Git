0f104af0c3b3769d49d063cfeda596c6b9ffea40
Clarisse Lerat/L3CDPN/2015 <clarisselerat@hotmail.fr>
2014-10-03 15:56:40 +0200

    camille repo

--- src/application.bat
+++ src/application.bat
@@ -1,4 +1,4 @@
-### application.bat - 201x - Camille Paviot ###
+### application.bat - 2014 - Camille Paviot Clarisse Lerat ###
 version="2"
 build_hour="11H00"
 echo "Copyright CamillePaviot - 201x"
